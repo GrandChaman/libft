855b01a23a0d1f6fe63aa31d2d08856538fb798d
Francis Le Roy <francis.leroy@protonmail.ch>
2017-11-01 22:38:04 +0100

    Much better ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bluff <bluff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/27 11:33:35 by bluff             #+#    #+#             */
-/*   Updated: 2017/10/27 11:38:27 by bluff            ###   ########.fr       */
+/*   Updated: 2017/11/01 22:34:49 by bluff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char *res;
 
 	i = 0;
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1);
-	len += ft_strlen(s2);
+	len = 0;
+	if (s1)
+		len += ft_strlen(s1);
+	if (s2)
+		len += ft_strlen(s2);
 	if (!(res = (char*)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	while (*s1)
+	while (s1 && *s1)
 		res[i++] = *(s1++);
-	while (*s2)
+	while (s2 && *s2)
 		res[i++] = *(s2++);
 	res[len] = '\0';
 	return (res);
